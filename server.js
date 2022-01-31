fd1ce4d1201bf898ec7a4eef23e1389b027e80e5
Leslie Patino <lesliejpatino@gmail.com>
2022-01-30 22:57:48 -0800

    created a post request that will save the new note entered by the user

--- server.js
+++ server.js
@@ -21,8 +21,22 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-    // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
-    
+    // POST /api/notes should receive a new note to save on the request body, add it to the db.json file
+
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text
+        }
+// must give the new note a unique ID (npm package)
+// must return new note to the client
+        readAndAppend(newNote, 'db/db.json');
+        res.json('Note added successfully!');
+    } else {
+        res.error('Error in adding note. Please try again');
+    }   
 });
 
 
